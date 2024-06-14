b5971b4dce3302840167e0458ac9c42bdf50e942
Vishesh Dadhich <visheshdadhich20@gmail.com>
2024-06-15 01:33:37 +0530

    Final Changes

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ export default function App() {
     setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
     setShowAddFriend(false);
   }
+  function handleSplitBill(value) {
+    setFriends(friends => friends.map(friend => friend.id === selectedFriend.id ? { ...friend, balance: friend.balance + value } : friend))
+    setSelectedFriend(null);
+  }
   return <div className="app">
     <div className='sidebar'>
       <FriendsList
@@ -31,6 +35,6 @@ export default function App() {
       <Button handleOnClick={handleShow}>{showAddFriend ? "Close" : "Add Friends"}</Button>
 
     </div>
-    {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
+    {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} />}
   </div>
 }
